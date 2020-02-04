756ca4c85dd0c239e01ca5dab92c87e108924bba
Beatrice <beatrice.opara@softwire.com>
2020-02-04 14:30:34 +0000

    Read function

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.IO;
+
+namespace EmailExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Training\EmailExtraction\EmailExtraction\Data\sample.txt";
+            string text = ReadFile(path);
+            Console.WriteLine(text);
+        }
+        /*start with main
+         defined path to where file is 
+         call own function called ReadFile
+         saving its repsonse to variable called text
+         then print text 
+         */
+        private static string ReadFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            return text;
+        }
+        /*
+         created new function called ReadFile
+         defined a variable call path to where the file is
+         called func called File.ReadAll and pass var path in barckets
+         saved it into var text 
+         then return txt
+         */
+    }
+}
